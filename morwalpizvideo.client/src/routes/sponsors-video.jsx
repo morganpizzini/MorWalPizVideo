99d02300f8c5ad142f45f407ae8347d10f80010e
Morgan Pizzini <morgan.pizzini@icubed.it>
2025-02-08 22:58:39 +0100

    fix-spacing

--- morwalpizvideo.client/src/routes/sponsors-video.jsx
+++ morwalpizvideo.client/src/routes/sponsors-video.jsx
@@ -6,12 +6,14 @@ export default function Matches() {
             <div id="page-container" className="p-4 bg-white">
                 <h1 className="page-title">Video di presentazione</h1>
                 <hr />
+                <p><b>IT</b>: Attiva l'audio nel video, se non ti &#232; possibile l'ascolto trovi il documento riassuntivo sotto </p>
+                <p><b>EN</b>: Turn on video's audio, if you are unable to listen you can find the summary below</p>
                 <iframe width="100%" height="450px" className="rounded" src="https://www.youtube.com/embed/N4ndkYb7uFI?autoplay=1&mute=1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>    
                 <div className="page-text text-center">
-                    <a target="_blank" rel="noopener noreferrer" className="btn btn-primary me-2" href="https://morwalpizblob.blob.core.windows.net/page-images/sponsor-video/Morgan-Pizzini-CV.pdf" download>
-                        Scarica il recap (IT)
+                    <a target="_blank" rel="noopener noreferrer" className="btn btn-primary me-2 mt-2" href="https://morwalpizblob.blob.core.windows.net/page-images/sponsor-video/Morgan-Pizzini-CV.pdf" download>
+                        Scarica il riassunto (IT)
                     </a>
-                    <a target="_blank" rel="noopener noreferrer" className="btn btn-primary" href="https://morwalpizblob.blob.core.windows.net/page-images/sponsor-video/Morgan-Pizzini-CV-EN.pdf" download>
+                    <a target="_blank" rel="noopener noreferrer" className="btn btn-primary mt-2" href="https://morwalpizblob.blob.core.windows.net/page-images/sponsor-video/Morgan-Pizzini-CV-EN.pdf" download>
                         Download recap (EN)
                     </a>
                 </div>
